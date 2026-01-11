e852e3ae214b2c46cfa53e2f1faa3a07d23784af
veerababuveera1-collab <veerababu.veera1@gmail.com>
2026-01-12 02:26:02 +0530

    Create users.py

---
+++ users.py
@@ -0,0 +1,16 @@
+# users.py
+
+USERS = {
+    "doctor1": {
+        "password": "doctor123",
+        "role": "doctor"
+    },
+    "researcher1": {
+        "password": "research123",
+        "role": "researcher"
+    },
+    "admin": {
+        "password": "admin123",
+        "role": "admin"
+    }
+}
